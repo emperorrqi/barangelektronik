478dad4f0495fa8e681be21b519b4fd90bf25bb0
emperorrqi <rizkikamal1102@gmail.com>
2025-12-09 17:37:12 +0700

    Initial commit - clean config, use .env

--- app/config.php
+++ app/config.php
@@ -1,23 +1,13 @@
 <?php
-// Ambil environment variables (aman & wajib untuk Aiven / Docker)
-$DB_HOST = getenv("DB_HOST");
-$DB_USER = getenv("DB_USER");
-$DB_PASS = getenv("DB_PASS");
-$DB_NAME = getenv("DB_NAME");
-$DB_PORT = getenv("DB_PORT");
+$host = getenv('AIVEN_HOST');
+$user = getenv('AIVEN_USER');
+$pass = getenv('AIVEN_PASSWORD');
+$db   = getenv('AIVEN_DATABASE');
+$port = getenv('AIVEN_PORT');
 
-// Jika ada SSL CA (untuk Aiven pemakaian wajib)
-$DB_SSL_CA = getenv("DB_SSL_CA") ?: "/etc/ssl/certs/ca-certificates.crt";
+$koneksi = new mysqli($host, $user, $pass, $db, $port);
 
-// Buat koneksi MySQL dengan SSL (Aiven membutuhkan SSL REQUIRED)
-$mysqli = mysqli_init();
-
-// Aktifkan SSL untuk Aiven
-mysqli_ssl_set($mysqli, NULL, NULL, $DB_SSL_CA, NULL, NULL);
-
-// Koneksi ke database
-if (!mysqli_real_connect($mysqli, $DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, (int)$DB_PORT, NULL, MYSQLI_CLIENT_SSL)) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+if ($koneksi->connect_error) {
+    die("Gagal koneksi ke database: " . $koneksi->connect_error);
 }
-
 ?>
